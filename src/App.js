d6aaf2ee3f1e83646c173b8b5c5d2f0b861e693d
JDASL <127839733+JDASL@users.noreply.github.com>
2025-02-06 22:24:17 +0100

    added css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,29 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather />
+        <footer>
+          This weather app was created by
+          <a
+            href="https://www.instagram.com/yourgurljd/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <em>JD Apirinya </em>{" "}
+          </a>
+          with open-sourced code on
+          <a
+            href="https://github.com/JDASL/weather-react-week-5.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <em>GitHub</em>
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -9,29 +9,37 @@ export default function Weather() {
           <div className="col-9">
             <input
               type="search"
-              placeholder="Please write a city"
+              placeholder="Please write a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-dark w-100"
+            />
           </div>
         </div>
       </form>
       <h1>Oslo</h1>
       <ul>
-        <li>Oslo</li>
+        <li>Thursday</li>
+        <li>21:27</li>
         <li>Partly cloudly</li>
       </ul>
-      <div className="row">
+      <div className="row mt-2">
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Partyly cloudly"
-          ></img>
-          -4 ºC
+            className="float-left"
+          />
+          <span className="temperature">4</span>
+          <span className="unit">ºC</span>
         </div>
-        <div className="col-6">
+        <div className="col-6 mt-3">
           <ul>
             <li>Precipitation: 3%</li>
             <li>Humidity: 80%</li>
